0143c3002a7b9e93277ced4ada9f4cc52e03f852
matheus-sousa-nascimento <143366856+matheus-sousa-nascimento@users.noreply.github.com>
2025-05-07 10:41:02 -0300

    Add files via upload

---
+++ TabuladaDoOito.java
@@ -0,0 +1,21 @@
+/*
+ * TabuladaDoOito.java
+ * 
+ * 
+ * 
+ */
+
+public class TabuladaDoOito {
+	
+	public static void main (String[] args) {
+		//Matheus de sousa do nascimento
+		
+		System.out.println("A tabulada do 8");
+		
+		for(int n = 0;n <= 10;n++){
+			int mult = 8 * n;
+			System.out.println("8 x "+n+" = "+mult);
+		}
+	}
+}
+
